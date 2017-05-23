a12cd0042b4fb6248faa2db81a4b36ac4daedc24
Michael Amoabeng <michael.amoabeng2015@gmail.com>
2017-05-24 02:49:21 +0300

    The create invoice url end point has been and post data returns a successful response

--- invoiceVirtualEnv/invoices/invoicesapp/views.py
+++ invoiceVirtualEnv/invoices/invoicesapp/views.py
@@ -5,14 +5,25 @@ from invoicesapp.models import Invoice
 from invoicesapp.serializers import InvoiceSerializer
 
 #Use the decorator to specify which methods the invoice_list function is going to handle
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 #Function to return a list of invoices
 def invoice_list(request):
     if request.method == 'GET':
         invoices = Invoice.objects.all()
         serializer = InvoiceSerializer(invoices, many = True)
         return Response(serializer.data)
+    #Handling a post request
+    elif request.method == 'POST':
+        serializer = InvoiceSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_404_BAD_REQUEST)
 
+
+
+
+#Get a single invoice details
 @api_view(['GET'])
 def invoice_detail(request, pk):
     try:
